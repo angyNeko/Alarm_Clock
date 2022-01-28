fe737d7acb970e19b38abd6485b158e4fc4be9da
Nekofi <58923014+LolisDisWae@users.noreply.github.com>
2022-01-28 12:05:06 +0800

    Some Changes

--- app/src/main/java/com/example/alarmclock/MainActivity.java
+++ app/src/main/java/com/example/alarmclock/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.alarmclock;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -39,8 +40,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent toSetAlamr = new Intent(MainActivity.this, SetAlarmActivity.class);
+                startActivity(toSetAlamr);
             }
         });
     }
